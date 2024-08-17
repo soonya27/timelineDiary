f197439f6dbd2c04d7c6ba6f62605e193ef10763
soyeong <psykor48@gmail.com>
2024-08-17 15:34:30 +0900

    navbar css 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,11 +15,11 @@ const navBarList = [
 export default function Navbar() {
     const [nav, setNav] = useState(navBarList[0].text);
     return (
-        <header className='md:border-b-none md:border-r-2 border-r-none border-b-2'>
+        <header className='md:border-b-none md:border-r-2 md:px-3 border-r-none border-b-2 md:basis-1/5'>
             <div className='md:block hidden'>
                 <Profile />
             </div>
-            <ul className='flex flex-row md:flex-col justify-between px-3'>
+            <ul className='flex flex-row md:flex-col justify-between'>
                 {navBarList.map(({ link, text, icon }, idx) => (
                     <li key={idx} onClick={() => setNav(text)}><Link className={`flex items-center p-2 ${text === nav && 'text-blue-950 font-bold'}`} to={link}>{icon} {text}</Link></li>
                 ))}
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 import FloatingMenu from '../components/FloatingMenu';
 
 
 
 export default function Main() {
     let { pathname } = useLocation();
+
     return (
         <div className='flex flex-col md:flex-row h-screen'>
-            <Navbar />
+            {pathname !== '/new' && <Navbar />}
             <div className='flex-grow-[1] flex-shrink-[1] basis-auto overflow-y-auto p-3'>
                 <Outlet />
             </div>
